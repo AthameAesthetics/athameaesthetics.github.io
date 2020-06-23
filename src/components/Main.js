6a531a6ccddb350737033bb09983b089b3052bdd
patrickwitmer <patrick@witmerdesigns.com>
2020-06-22 20:06:42 -0600

    social media links gone

--- src/components/Main.js
+++ src/components/Main.js
@@ -273,8 +273,8 @@ class Main extends React.Component {
               </li>
             </ul>
           </form>
-          <ul className="icons">
-            {/* <li>
+          {/* <ul className="icons"> */}
+          {/* <li>
               <a
                 href="https://twitter.com/HuntaroSan"
                 className="icon fa-twitter"
@@ -282,7 +282,7 @@ class Main extends React.Component {
                 <span className="label">Twitter</span>
               </a>
             </li> */}
-            {/* <li>
+          {/* <li>
               <a href="https://codebushi.com" className="icon fa-facebook">
                 <span className="label">Facebook</span>
               </a>
@@ -292,7 +292,7 @@ class Main extends React.Component {
                 <span className="label">Instagram</span>
               </a>
             </li> */}
-            {/* <li>
+          {/* <li>
               <a
                 href="https://github.com/codebushi/gatsby-starter-dimension"
                 className="icon fa-github"
@@ -300,7 +300,7 @@ class Main extends React.Component {
                 <span className="label">GitHub</span>
               </a>
             </li> */}
-          </ul>
+          {/* </ul> */}
           {close}
         </article>
       </div>
